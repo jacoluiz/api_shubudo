19594a04faf1f81039128adfb4ec5cdf13d85d40
jacoluiz <jacochandeski@gmail.com>
2024-03-10 21:28:45 -0300

    inicio da adicao de novos models

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import programacao from "./programacaoRoutes.js"
 
 const routes = (app) => {
-    app.route("/").get((req, res) => res.status(200).sand("index"));
-
     app.use(express.json(), programacao);
 };
 
