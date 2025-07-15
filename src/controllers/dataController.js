88928599563833fab4f68be885242af16497dfad
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-07-15 13:18:02 -0300

    Deleção e edição de eventos

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -25,6 +25,45 @@ class dateController {
       });
     }
   }
+
+  static async atualizarData(req, res) {
+    try {
+      const { id } = req.params;
+      const dataAtualizada = await DateEvent.findByIdAndUpdate(id, req.body, { new: true });
+
+      if (!dataAtualizada) {
+        return res.status(404).json({ message: "Data não encontrada" });
+      }
+
+      res.status(200).json({
+        message: "Data atualizada com sucesso",
+        data: dataAtualizada
+      });
+    } catch (erro) {
+      res.status(500).json({
+        message: `${erro.message} - Erro ao atualizar data`
+      });
+    }
+  }
+
+  static async deletarData(req, res) {
+    try {
+      const { id } = req.params;
+      const dataRemovida = await DateEvent.findByIdAndDelete(id);
+
+      if (!dataRemovida) {
+        return res.status(404).json({ message: "Data não encontrada" });
+      }
+
+      res.status(200).json({
+        message: "Data deletada com sucesso"
+      });
+    } catch (erro) {
+      res.status(500).json({
+        message: `${erro.message} - Erro ao deletar data`
+      });
+    }
+  }
 }
 
 export default dateController;
--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -5,5 +5,7 @@ const dateRoutes = express.Router();
 
 dateRoutes.get("/datas", dateController.listarDatas);
 dateRoutes.post("/datas", dateController.criarData);
+dateRoutes.put("/datas/:id", dateController.atualizarData);
+dateRoutes.delete("/datas/:id", dateController.deletarData);
 
 export default dateRoutes;
