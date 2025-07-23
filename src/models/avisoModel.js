f6e74c2339bb1fbf4ec87dcf4fc1760c436670e5
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-07-23 16:06:30 -0300

    Avisos revivido

--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -4,7 +4,7 @@ const avisoSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   conteudo: { type: String, required: true },
   dataHoraCriacao: { type: Date, default: Date.now }, // define data/hora de criação automaticamente
-  publicoAlvo: { type: String, required: true }
+  publicoAlvo: { type: [String], default: [] }
 }, { versionKey: false });
 
 const Aviso = mongoose.model("avisos", avisoSchema);
