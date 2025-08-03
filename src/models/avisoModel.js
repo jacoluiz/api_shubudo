a470e13482e5f26973afd35e97a4a8f0a47222cd
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-08-03 20:08:41 -0300

    Adição de novo campo em avisos

--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -4,7 +4,8 @@ const avisoSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   conteudo: { type: String, required: true },
   dataHoraCriacao: { type: Date, default: Date.now }, // define data/hora de criação automaticamente
-  publicoAlvo: { type: [String], default: [] }
+  publicoAlvo: { type: [String], default: [] },
+  academia: { type: String }
 }, { versionKey: false });
 
 const Aviso = mongoose.model("avisos", avisoSchema);
