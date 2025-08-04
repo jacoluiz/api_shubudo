e903e6e21e0df2fd53428f6e52b8e2b5f5e190c4
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-08-03 21:41:53 -0300

    academiaId e filialId no usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -13,6 +13,8 @@ const usuarioSchema = new mongoose.Schema({
     status: { type: String, default: "ativo" },
     dan: { type: Number, default: 0 },
     academia: { type: String },
+    academiaId: {type: String},
+    filialId: {type: String},
     tamanhoFaixa: { type: String },
     lesaoOuLaudosMedicos: { type: String },
     registroAKSD: { type: String },
