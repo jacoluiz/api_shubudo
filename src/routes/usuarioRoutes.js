1447c2f735f8c96bcce6050359d248ae826790b7
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-08-04 19:24:12 -0300

    ordem das rotas

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -5,24 +5,16 @@ const routes = express.Router();
 
 // Lista todos os usuários
 routes.get("/usuarios", usuarioController.listarUsuarios);
-
+routes.get("/usuarios/faixa", usuarioController.buscarFaixaDoUsuario);
+routes.get("/usuarios/por-academia", usuarioController.listarUsuariosPorAcademia);
 // Busca usuário por ID
 routes.get("/usuarios/:id", usuarioController.buscarUsuarioPorId);
-
 // Cadastra um novo usuário
 routes.post("/usuarios", usuarioController.cadastrarUsuario);
-
 // Atualiza os dados de um usuário
 routes.put("/usuarios/:id", usuarioController.atualizarUsuario);
-
 // Exclui um usuário
 routes.delete("/usuarios/:id", usuarioController.excluirUsuario);
-
-// Busca a faixa de um usuário (usando ID ou email)
-routes.get("/usuarios/faixa", usuarioController.buscarFaixaDoUsuario);
-
-routes.get("/usuarios/por-academia", usuarioController.listarUsuariosPorAcademia);
-
 routes.post("/usuarios/login", usuarioController.login);
 
 export default routes;
