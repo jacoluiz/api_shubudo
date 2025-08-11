b834e81fab277b701ff1df1f4edf09d86f3215ce
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-08-10 22:01:12 -0300

    Adição de notas

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -13,13 +13,35 @@ const usuarioSchema = new mongoose.Schema({
     status: { type: String, default: "ativo" },
     dan: { type: Number, default: 0 },
     academia: { type: String },
-    academiaId: {type: String},
-    filialId: {type: String},
+    academiaId: { type: String },
+    filialId: { type: String },
     tamanhoFaixa: { type: String },
     lesaoOuLaudosMedicos: { type: String },
     registroAKSD: { type: String },
     fcmToken: { type: String },
-    professorEm: { type: [String], default: [] }
+    professorEm: { type: [String], default: [] },
+    notas: {
+        type: [{
+            evento: { type: String },
+            faixaAvaliada: { type: String },
+            avaliador: { type: String },
+            atquesDeMao: { type: String },
+            defesas: { type: String },
+            chutes: { type: String },
+            defesaPessoal: { type: String },
+            sequenciaDeCombate: { type: String },
+            katas: { type: String },
+            defesaExtraBanner: { type: String },
+            sequenciaDeAmarela: { type: String },
+            rolamentosEQuedas: { type: String },
+            projecoes: { type: String },
+            armas: { type: String },
+            defesaDeArma: { type: String },
+            saidaDeEstrangulamento: { type: String },
+            tecnicasDeChao: { type: String },
+        }],
+        default: []
+    }
 }, { versionKey: false });
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
